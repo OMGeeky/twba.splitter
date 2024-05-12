f38c7bcceafad75f8e58dfeea95ef8a7aeb70b82
OMGeeky <aalaalgmx@gmail.com>
2024-05-12 19:03:29 +0200

    update to latest twba-common

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+use crate::prelude::*;
 use std::path::PathBuf;
 use tokio::io;
 
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,6 @@
 use crate::errors::SplitterError;
 pub(crate) use std::result::Result as StdResult;
-
+pub(crate) use twba_common::prelude::*;
 pub type Result<T> = StdResult<T, SplitterError>;
 
 pub(crate) use tracing::{debug, error, info, trace, warn};
